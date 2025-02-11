1c5356722a1b66b7558b294184d19400437c2b93
zeropse <129523087+zeropse@users.noreply.github.com>
2025-02-12 01:54:55 +0530

    added home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center pt-10">BCA MADE EASY</h1>
+      <p className="text-lg text-center pt-5">
+        This allows you to view BCA Class 3C notes and other resources.
+      </p>
+      <div className="flex justify-center pt-10">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer hover:opacity-80">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
